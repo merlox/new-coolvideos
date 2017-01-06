07b2f9fd0af22c4927e8c8d0bf07a6407f6c3869
merlox <merloxdixcontrol@gmail.com>
2017-01-06 21:55:40 +0100

    This

--- server.js
+++ server.js
@@ -17,8 +17,11 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log('Server started');
-	functions.generateAllOnlineSnapshots((err) => {
-		if(err) console.log(err);
-	});
+        console.log('Server started');
+        functions.deleteExistingSnapshots(err => {
+                if(err) console.log(err);
+        });
+        functions.generateAllSnapshots((err) => {
+                if(err) console.log(err);
+        });
 });
--- server/functions.js
+++ server/functions.js
@@ -30,24 +30,24 @@ function copyFile(origin, end, fileName, callback){
 };
 //To return the snapshots of all videos as an array callback not generate images
 function getAllSnapshots(cb){
-	if(cb == null){
-		cb = () => {};
-	}
-	var snapshotsLocation = path.join(__dirname, 'uploads/snapshots');
-	var snapshots = [];
-	fs.readdir(snapshotsLocation, (err, images) => {
-		images.forEach((image, index) => {
-			snapshots.push(image);
-			var origin = path.join(__dirname, '/uploads/snapshots/', image);
-			var end = path.join(__dirname, '../public/public-uploads/');
-			copyFile(origin, end, image, (err) => {
-				if(err) console.log(err);
-			});
-			if(index + 1 >= images.length){
-				return cb(snapshots);
-			}
-		});
-	});
+        if(cb == null){
+                cb = () => {};
+        }
+        var snapshotsLocation = path.join(__dirname, 'uploads/snapshots');
+        var snapshots = [];
+        fs.readdir(snapshotsLocation, (err, images) => {
+                images.forEach((image, index) => {
+                        snapshots.push(image);
+                        var origin = path.join(__dirname, '/uploads/snapshots/', image);
+                        var end = path.join(__dirname, '../public/public-uploads/');
+                        copyFile(origin, end, image, (err) => {
+                                if(err) console.log(err);
+                        });
+                        if(index + 1 >= images.length){
+                                return cb(snapshots);
+                        }
+                });
+        });
 };
 //To generate snapshots of online videos
 function getOnlineSnapshot(url, cb){
@@ -63,34 +63,63 @@ function getOnlineSnapshot(url, cb){
 		}
 	});
 };
+
+//Generate snapshot image
+function generateSnapshot(videoName, cb){
+        var snapshotName = videoName.replace(/(\.mp4)+/, '.jpg');
+        var snapshotPathAndName = path.join(__dirname, '/uploads/snapshots', snapshotName);
+        cp.exec(`ffmpeg -y -ss 00:01:35 -i ${videoName} -vframes 1 ${snapshotPathAndName}`, (err, stdout, stderr) => {
+                console.log('done');
+                if(err) return cb(err);
+                return cb(null);
+        }
+};
+
+function deleteExistingSnapshots(cb){
+        fs.readdir(path.join(__dirname, 'uploads/snapshots'), (err, files) => {
+                if(err) return console.log(err);
+                var counter = 0;
+                files.forEach(file => {
+                        fs.unlink(path.join(__dirname, 'upload/snapshots', file), (err) => {
+                                if(err) console.log(err);
+                                counter++;
+                                if(counter >= files.length){
+                                        return cb(null);
+                                }
+                        });
+                });
+        });
+};
 //To generate all the online videos snapshots
 function generateAllOnlineSnapshots(cb){
-	var error = "";
-	fs.readFile(path.join(__dirname, 'videonames.txt'), (err, data) => {
-		if(err) return cb(err);
-		var videoNames = data.toString();
-		var re = /(http:\/\/.*\.mp4)/gm;
-		var videoNamesArray = [];
-		var finalVideoNamesArray = [];
-		//Cada vez que se ejecuta el regex sale el e mantensiguiente resultado y se almacena siempre en el array[0]
-		//con lo que hay que crear un nuevo array quga esos resultados
-		while((videoNamesArray = re.exec(videoNames)) != null){
-			finalVideoNamesArray.push(videoNamesArray[0]);
-		}
-		finalVideoNamesArray.forEach((video, index) => {
-			getOnlineSnapshot(video, (err) => {
-				if(err) error = err;
-			});
-			if(index + 1 >= finalVideoNamesArray.length){
-				if(error) return cb(error);
-				else return cb(null);
-			}
-		});
-	});
+        var error = "";
+        fs.readFile(path.join(__dirname, 'videonames.txt'), (err, data) => {
+                if(err) return cb(err);
+                var videoNames = data.toString();
+                var re = /(http:\/\/.*\.mp4)/gm;
+                var videoNamesArray = [];
+                var finalVideoNamesArray = [];
+                //Cada vez que se ejecuta el regex sale el e mantensiguiente resultado y se almacena siempre en el array[0]
+                //con lo que hay que crear un nuevo array quga esos resultados
+                while((videoNamesArray = re.exec(videoNames)) != null){
+                        finalVideoNamesArray.push(videoNamesArray[0]);
+                }
+                finalVideoNamesArray.forEach((video, index) => {
+                        getOnlineSnapshot(video, (err) => {
+                                if(err) error = err;
+                        });
+                        if(index + 1 >= finalVideoNamesArray.length){
+                                if(error) return cb(error);
+                                else return cb(null);
+                        }
+                });
+        });
 };
 module.exports = {
-	copyFile: copyFile,
-	getAllSnapshots: getAllSnapshots,
-	generateAllOnlineSnapshots: generateAllOnlineSnapshots,
-	getOnlineSnapshot: getOnlineSnapshot
+        copyFile: copyFile,
+        getAllSnapshots: getAllSnapshots,
+        generateAllOnlineSnapshots: generateAllOnlineSnapshots,
+        getOnlineSnapshot: getOnlineSnapshot,
+        deleteExistingSnapshots: deleteExistingSnapshots,
+        generateAllSnapshots: generateAllSnapshots
 };
