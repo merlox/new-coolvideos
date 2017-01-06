241d17d2b1624e42a8b46a319993b8604b78fad6
merlox <merloxdixcontrol@gmail.com>
2017-01-06 22:49:42 +0100

    Fixed bug next()

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('*', (req, res, next) => {
 	console.log(`Req: ${req.url} from ${req.ip}`);
+	next();
 });
 
 app.use('/api', apiRoutes);
