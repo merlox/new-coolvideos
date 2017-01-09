9de9c35a69873cb7772af18b37398f6af52be795
merlox <merloxdixcontrol@gmail.com>
2017-01-09 17:25:41 +0100

    Now ads started to work again

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var express = require('express'),
 	app = express(),
 	apiRoutes = require('./server/apiRoutes.js'),
 	port = (process.env.PORT || 9000),
-	functions = require('./server/functions.js'),
-	fs = require('fs'),
-	http = require('http');
+	functions = require('./server/functions.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('*', (req, res, next) => {
 	console.log(`Req: ${req.originalUrl} from ${req.ip}`);
+	next();
 });
 
 app.use('/api', apiRoutes);
