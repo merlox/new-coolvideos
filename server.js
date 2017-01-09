9fce14700f2d38dfb119c66e2ac23df32354e5dc
merlox <merloxdixcontrol@gmail.com>
2017-01-09 15:17:40 +0000

    a

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ PUBLIC ROUTES HERE
 
 */
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/start.html');
+	res.sendFile(path.join(__dirname, '/public/start.html'));
 });
 
 app.listen(port, (req, res) => {
