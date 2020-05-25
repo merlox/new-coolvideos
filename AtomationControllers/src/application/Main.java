1428d423b7ee3d9119f3371be5731abd1061f669
Onayngo Benard <61835307+benardonyango-oss@users.noreply.github.com>
2020-05-26 02:08:06 +0300

    Creating the interface with automation controls

---
+++ AtomationControllers/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception  {
+		
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/Main-Control.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Twitter Automation");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	
+	}
+	
+	
+}
